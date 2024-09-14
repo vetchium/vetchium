28a8c2370d6d62cc855c5c14a8276d16c8f5439e
Sankar சங்கர் <sankar.curiosity@gmail.com>
2024-09-14 18:38:55 +0530

    signin changes for web

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { Typography } from "antd";
-import { useTranslation } from "react-i18next";
+import { Button, Typography } from 'antd';
+import { useTranslation } from 'react-i18next';
 
 const { Title, Paragraph } = Typography;
 
-const Home: React.FC = () => {
+function Home({ onSignOut }: { onSignOut: () => void }) {
   const { t } = useTranslation();
 
   return (
     <div>
-      <Title>{t("home.welcome")}</Title>
-      <Paragraph>{t("home.description")}</Paragraph>
+      <Title>{t('home.welcome')}</Title>
+      <Paragraph>{t('home.description')}</Paragraph>
+      <Button onClick={onSignOut}>{t('home.signOut')}</Button>
     </div>
   );
-};
+}
 
 export default Home;
--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Typography } from 'antd';
-import { useTranslation } from 'react-i18next';
+import { Button, Typography } from "antd";
+import { useTranslation } from "react-i18next";
 
 const { Title } = Typography;
 
-const Login: React.FC = () => {
+function Login({ onSignIn }: { onSignIn: () => void }) {
   const { t } = useTranslation();
 
   return (
     <div>
-      <Title level={2}>{t('login.title')}</Title>
+      <Title level={2}>{t("login.title")}</Title>
+      <Button onClick={onSignIn}>{t("login.signIn")}</Button>
       {/* Add login form here */}
     </div>
   );
-};
+}
 
 export default Login;
