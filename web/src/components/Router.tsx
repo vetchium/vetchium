de94efaecee55fc32cf01e0afc571069bd6f1c0c
Sankar சங்கர் <sankar.curiosity@gmail.com>
2024-09-18 10:58:45 +0530

    user profile

--- web/src/components/Router.tsx
+++ web/src/components/Router.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
 import MyProfile from "../pages/MyProfile";
 import AddWorkHistory from "../pages/AddWorkHistory";
+import UserProfile from "../pages/UserProfile";
 
 function Router() {
   return (
@@ -9,6 +10,7 @@ function Router() {
       <Route path="/home" element={<Dashboard />} />
       <Route path="/my-profile" element={<MyProfile />} />
       <Route path="/add-work-history" element={<AddWorkHistory />} />
+      <Route path="/u/:id" element={<UserProfile />} />
     </Routes>
   );
 }
