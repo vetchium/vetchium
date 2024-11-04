d1eb3fa8207894a74f14f8c1d4afcf1b068c0254
Sankar சங்கர் <sankar.curiosity@gmail.com>
2024-11-04 16:51:11 +0530

    Cleanup mailpit on launch

--- dolores/dolores_suite_test.go
+++ dolores/dolores_suite_test.go
@@ -1,6 +1,7 @@
 package dolores
 
 import (
+	"net/http"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -11,3 +12,10 @@ func TestDolores(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Dolores Suite")
 }
+
+var _ = BeforeSuite(func() {
+	// Delete all mails on mailpit to clean up from past runs
+	resp, err := http.DefaultClient.Get("http://localhost:8025/api/v1/messages")
+	Expect(err).ShouldNot(HaveOccurred())
+	Expect(resp.StatusCode).Should(Equal(http.StatusOK))
+})
