cb2c831807aabbfcf1b4505aae17c70c7b72db1a
Sankar சங்கர் <sankar.curiosity@gmail.com>
2024-11-15 13:56:15 +0530

    drop extra code

--- api/pkg/vetchi/common.go
+++ api/pkg/vetchi/common.go
@@ -1,7 +1,15 @@
 package vetchi
 
 type CountryCode string
-
+type Currency string
+type EmailAddress string
+type Password string
 type TimeZone string
 
-type Currency string
+type ValidationErrors struct {
+	Errors []string `json:"errors"`
+}
+
+type EmployerSignInResponse struct {
+	Token string `json:"token"`
+}
---
+++ api/pkg/vetchi/employer-auth.go
@@ -0,0 +1,39 @@
+package vetchi
+
+type EmployerSignInRequest struct {
+	ClientID string       `json:"client_id" validate:"required,client_id"`
+	Email    EmailAddress `json:"email"     validate:"required,email"`
+	Password Password     `json:"password"  validate:"required,password"`
+}
+
+type EmployerTFARequest struct {
+	TFACode    string `json:"tfa_code"              validate:"required"`
+	TFAToken   string `json:"tfa_token"             validate:"required"`
+	RememberMe bool   `json:"remember_me,omitempty"`
+}
+
+type EmployerTFAResponse struct {
+	SessionToken string `json:"session_token"`
+}
+
+type GetOnboardStatusRequest struct {
+	ClientID string `json:"client_id" validate:"required,client_id"`
+}
+
+type GetOnboardStatusResponse struct {
+	Status OnboardStatus `json:"status"`
+}
+
+type OnboardStatus string
+
+const (
+	DomainNotVerified            OnboardStatus = "DOMAIN_NOT_VERIFIED"
+	DomainVerifiedOnboardPending OnboardStatus = "DOMAIN_VERIFIED_ONBOARD_PENDING"
+	DomainOnboarded              OnboardStatus = "DOMAIN_ONBOARDED"
+)
+
+type SetOnboardPasswordRequest struct {
+	ClientID string   `json:"client_id" validate:"required,client_id"`
+	Password Password `json:"password"  validate:"required,password"`
+	Token    string   `json:"token"     validate:"required"`
+}
