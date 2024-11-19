3eca59f8e0f6e7e0aaa73989bd79d109b57f9deb
Sankar சங்கர் <sankar.curiosity@gmail.com>
2024-11-19 10:33:50 +0530

    logout handler

---
+++ api/internal/hermione/hubauth/logout.go
@@ -0,0 +1,32 @@
+package hubauth
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/psankar/vetchi/api/internal/wand"
+)
+
+func LogoutHandler(h wand.Wand) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		h.Dbg("Entered hub logout")
+
+		token := r.Header.Get("Authorization")
+		if token == "" {
+			h.Dbg("no token found")
+			http.Error(w, "", http.StatusUnauthorized)
+			return
+		}
+
+		token = strings.TrimPrefix(token, "Bearer ")
+
+		err := h.DB().Logout(r.Context(), token)
+		if err != nil {
+			h.Dbg("failed to logout", "error", err)
+			http.Error(w, "", http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	}
+}
