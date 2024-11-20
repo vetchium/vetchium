7024984f9366079e65d339c5d73dc54513e6b129
Sankar சங்கர் <sankar.curiosity@gmail.com>
2024-11-20 11:08:26 +0530

    print, colorize stacks on failure

--- dolores/dolores_suite_test.go
+++ dolores/dolores_suite_test.go
@@ -1,15 +1,51 @@
 package dolores
 
 import (
+	"fmt"
 	"net/http"
+	"runtime"
 	"testing"
 
+	"github.com/fatih/color"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
+func colorizeStackTrace() string {
+	pc := make([]uintptr, 10)
+	n := runtime.Callers(3, pc)
+	frames := runtime.CallersFrames(pc[:n])
+
+	stackTrace := ""
+	for {
+		frame, more := frames.Next()
+
+		stackTrace += color.BlueString(frame.Function) + "\n"
+		stackTrace += "\t" + color.GreenString(frame.File)
+		stackTrace += ":" + color.YellowString(
+			fmt.Sprintf("%d", frame.Line),
+		) + "\n"
+
+		if !more {
+			break
+		}
+	}
+	return stackTrace
+}
+
+func customFailHandler(message string, callerSkip ...int) {
+	red := color.New(color.FgRed).SprintFunc()
+	yellow := color.New(color.FgYellow).SprintFunc()
+
+	fmt.Println(red("Assertion failed:"), message)
+	fmt.Println(yellow("Stack trace:"))
+	fmt.Println(colorizeStackTrace())
+
+	Fail(message, callerSkip...)
+}
+
 func TestDolores(t *testing.T) {
-	RegisterFailHandler(Fail)
+	RegisterFailHandler(customFailHandler)
 	RunSpecs(t, "Dolores Suite")
 }
 
--- dolores/go.mod
+++ dolores/go.mod
@@ -3,6 +3,7 @@ module github.com/psankar/vetchi/dolores
 go 1.23.2
 
 require (
+	github.com/fatih/color v1.18.0
 	github.com/jackc/pgx/v5 v5.7.1
 	github.com/onsi/ginkgo/v2 v2.20.2
 	github.com/onsi/gomega v1.34.2
@@ -23,6 +24,8 @@ require (
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/rogpeppe/go-internal v1.13.1 // indirect
 	golang.org/x/crypto v0.28.0 // indirect
 	golang.org/x/net v0.28.0 // indirect
