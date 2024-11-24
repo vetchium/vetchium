5b5783b112d4f98c73bb2b12794cb9a638063d70
Sankar சங்கர் <sankar.curiosity@gmail.com>
2024-11-24 12:21:24 +0530

    TFA

--- dolores/0008-hub-openings_test.go
+++ dolores/0008-hub-openings_test.go
@@ -4,7 +4,11 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"net/url"
+	"regexp"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	. "github.com/onsi/ginkgo/v2"
@@ -38,7 +42,93 @@ var _ = FDescribe("Hub Openings", Ordered, func() {
 		var loginRespObj vetchi.LoginResponse
 		err = json.NewDecoder(loginResp.Body).Decode(&loginRespObj)
 		Expect(err).ShouldNot(HaveOccurred())
-		hubUserToken = loginRespObj.Token
+		tfaToken := loginRespObj.Token
+
+		// Get TFA code from email
+		baseURL, err := url.Parse(mailPitURL + "/api/v1/search")
+		Expect(err).ShouldNot(HaveOccurred())
+		query := url.Values{}
+		query.Add(
+			"query",
+			"to:hubopening@hub.example subject:Vetchi Two Factor Authentication",
+		)
+		baseURL.RawQuery = query.Encode()
+
+		var messageID string
+		for i := 0; i < 3; i++ {
+			<-time.After(10 * time.Second)
+			mailPitResp, err := http.Get(baseURL.String())
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(mailPitResp.StatusCode).Should(Equal(http.StatusOK))
+
+			body, err := io.ReadAll(mailPitResp.Body)
+			Expect(err).ShouldNot(HaveOccurred())
+
+			var mailPitRespObj MailPitResponse
+			err = json.Unmarshal(body, &mailPitRespObj)
+			Expect(err).ShouldNot(HaveOccurred())
+
+			if len(mailPitRespObj.Messages) > 0 {
+				messageID = mailPitRespObj.Messages[0].ID
+				break
+			}
+		}
+		Expect(messageID).ShouldNot(BeEmpty())
+
+		// Get the email content
+		mailResp, err := http.Get(mailPitURL + "/api/v1/message/" + messageID)
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(mailResp.StatusCode).Should(Equal(http.StatusOK))
+
+		body, err := io.ReadAll(mailResp.Body)
+		Expect(err).ShouldNot(HaveOccurred())
+
+		re := regexp.MustCompile(
+			`Your Two Factor authentication code is:\s*([0-9]+)`,
+		)
+		matches := re.FindStringSubmatch(string(body))
+		Expect(len(matches)).Should(BeNumerically(">=", 2))
+		tfaCode := matches[1]
+
+		// Clean up the email
+		deleteReqBody, err := json.Marshal(MailPitDeleteRequest{
+			IDs: []string{messageID},
+		})
+		Expect(err).ShouldNot(HaveOccurred())
+
+		req, err := http.NewRequest(
+			"DELETE",
+			mailPitURL+"/api/v1/messages",
+			bytes.NewBuffer(deleteReqBody),
+		)
+		Expect(err).ShouldNot(HaveOccurred())
+		req.Header.Set("Accept", "application/json")
+		req.Header.Add("Content-Type", "application/json")
+
+		deleteResp, err := http.DefaultClient.Do(req)
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(deleteResp.StatusCode).Should(Equal(http.StatusOK))
+
+		// Complete TFA flow
+		tfaReqBody, err := json.Marshal(vetchi.HubTFARequest{
+			TFAToken:   tfaToken,
+			TFACode:    tfaCode,
+			RememberMe: false,
+		})
+		Expect(err).ShouldNot(HaveOccurred())
+
+		tfaResp, err := http.Post(
+			serverURL+"/hub/tfa",
+			"application/json",
+			bytes.NewBuffer(tfaReqBody),
+		)
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(tfaResp.StatusCode).Should(Equal(http.StatusOK))
+
+		var tfaRespObj vetchi.HubTFAResponse
+		err = json.NewDecoder(tfaResp.Body).Decode(&tfaRespObj)
+		Expect(err).ShouldNot(HaveOccurred())
+		hubUserToken = tfaRespObj.SessionToken
 	})
 
 	AfterAll(func() {
