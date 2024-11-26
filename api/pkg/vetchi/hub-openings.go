17ca1f62945fbb2f2c285c030c11037517f3fc11
Sankar சங்கர் <sankar.curiosity@gmail.com>
2024-11-26 13:44:10 +0530

    Add validations

--- api/pkg/vetchi/hub-openings.go
+++ api/pkg/vetchi/hub-openings.go
@@ -23,17 +23,19 @@ func CountryCodePtr(c string) *CountryCode {
 }
 
 type FindHubOpeningsRequest struct {
-	OpeningTypes       []OpeningType    `json:"opening_types"        validate:"dive,omitempty,validate_opening_type"`
-	CompanyDomains     []string         `json:"company_domains"      validate:"dive,omitempty,validate_domain"`
-	ExperienceRange    *ExperienceRange `json:"experience_range"     validate:"omitempty"`
-	SalaryRange        *SalaryRange     `json:"salary_range"         validate:"omitempty"`
-	CountryCode        *CountryCode     `json:"country_code"         validate:"omitempty,validate_country_code"`
-	Cities             []string         `json:"cities"               validate:"dive,omitempty,min=1,max=100"`
-	MinEducationLevel  *EducationLevel  `json:"min_education_level"  validate:"omitempty,validate_education_level"`
-	RemoteTimezones    []TimeZone       `json:"remote_timezones"     validate:"dive,omitempty,validate_timezone"`
-	RemoteCountryCodes []CountryCode    `json:"remote_country_codes" validate:"dive,omitempty,validate_country_code"`
-	PaginationKey      int64            `json:"pagination_key"`
-	Limit              int64            `json:"limit"                validate:"min=1,max=100"`
+	CountryCode *CountryCode `json:"country_code" validate:"omitempty,validate_country_code"`
+	Cities      []string     `json:"cities"       validate:"dive,omitempty,min=1,max=4"`
+
+	OpeningTypes    []OpeningType    `json:"opening_types"    validate:"dive,omitempty,validate_opening_type"`
+	CompanyDomains  []string         `json:"company_domains"  validate:"dive,omitempty,validate_domain,max=4"`
+	ExperienceRange *ExperienceRange `json:"experience_range" validate:"omitempty"`
+	SalaryRange     *SalaryRange     `json:"salary_range"     validate:"omitempty"`
+
+	MinEducationLevel  *EducationLevel `json:"min_education_level"  validate:"omitempty,validate_education_level"`
+	RemoteTimezones    []TimeZone      `json:"remote_timezones"     validate:"dive,omitempty,validate_timezone,max=4"`
+	RemoteCountryCodes []CountryCode   `json:"remote_country_codes" validate:"dive,omitempty,validate_country_code,max=4"`
+	PaginationKey      int64           `json:"pagination_key"`
+	Limit              int64           `json:"limit"                validate:"min=1,max=100"`
 }
 
 type HubOpening struct {
