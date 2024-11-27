07565a27ee40e3a57ea1822a039741eb2a549d00
Sankar சங்கர் <sankar.curiosity@gmail.com>
2024-11-27 11:13:49 +0530

    Employer auth documentation

--- api/pkg/vetchi/employer-auth.go
+++ api/pkg/vetchi/employer-auth.go
@@ -1,5 +1,27 @@
 package vetchi
 
+type GetOnboardStatusRequest struct {
+	ClientID string `json:"client_id" validate:"required,client_id"`
+}
+
+type OnboardStatus string
+
+const (
+	DomainNotVerified            OnboardStatus = "DOMAIN_NOT_VERIFIED"
+	DomainVerifiedOnboardPending OnboardStatus = "DOMAIN_VERIFIED_ONBOARD_PENDING"
+	DomainOnboarded              OnboardStatus = "DOMAIN_ONBOARDED"
+)
+
+type GetOnboardStatusResponse struct {
+	Status OnboardStatus `json:"status"`
+}
+
+type SetOnboardPasswordRequest struct {
+	ClientID string   `json:"client_id" validate:"required,client_id"`
+	Password Password `json:"password"  validate:"required,password"`
+	Token    string   `json:"token"     validate:"required"`
+}
+
 type EmployerSignInRequest struct {
 	ClientID string       `json:"client_id" validate:"required,client_id"`
 	Email    EmailAddress `json:"email"     validate:"required,email"`
@@ -18,26 +40,4 @@ type EmployerTFARequest struct {
 
 type EmployerTFAResponse struct {
 	SessionToken string `json:"session_token"`
-}
-
-type GetOnboardStatusRequest struct {
-	ClientID string `json:"client_id" validate:"required,client_id"`
-}
-
-type GetOnboardStatusResponse struct {
-	Status OnboardStatus `json:"status"`
-}
-
-type OnboardStatus string
-
-const (
-	DomainNotVerified            OnboardStatus = "DOMAIN_NOT_VERIFIED"
-	DomainVerifiedOnboardPending OnboardStatus = "DOMAIN_VERIFIED_ONBOARD_PENDING"
-	DomainOnboarded              OnboardStatus = "DOMAIN_ONBOARDED"
-)
-
-type SetOnboardPasswordRequest struct {
-	ClientID string   `json:"client_id" validate:"required,client_id"`
-	Password Password `json:"password"  validate:"required,password"`
-	Token    string   `json:"token"     validate:"required"`
 }
