901bc71e323c8721fc96ae22d09ba69619dd2da9
Sankar சங்கர் <sankar.curiosity@gmail.com>
2024-12-02 21:58:06 +0530

    Set Application Color Tag

---
+++ api/internal/hermione/applications/set-application-color-tag.go
@@ -0,0 +1,51 @@
+package applications
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/psankar/vetchi/api/internal/db"
+	"github.com/psankar/vetchi/api/internal/wand"
+	"github.com/psankar/vetchi/api/pkg/vetchi"
+)
+
+func SetApplicationColorTag(h wand.Wand) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		h.Dbg("Entered SetApplicationColorTag")
+		var setApplicationColorTagReq vetchi.SetApplicationColorTagRequest
+		err := json.NewDecoder(r.Body).Decode(&setApplicationColorTagReq)
+		if err != nil {
+			h.Dbg("failed to decode request", "error", err)
+			http.Error(w, "", http.StatusBadRequest)
+			return
+		}
+
+		if !h.Vator().Struct(w, &setApplicationColorTagReq) {
+			h.Dbg("invalid request", "error", err)
+			return
+		}
+		h.Dbg("validated", "req", setApplicationColorTagReq)
+
+		err = h.DB().
+			SetApplicationColorTag(r.Context(), setApplicationColorTagReq)
+		if err != nil {
+			if errors.Is(err, db.ErrApplicationStateInCompatible) {
+				http.Error(w, "", http.StatusUnprocessableEntity)
+				return
+			}
+
+			if errors.Is(err, db.ErrNoApplication) {
+				http.Error(w, "", http.StatusNotFound)
+				return
+			}
+
+			h.Dbg("failed to add application color tag", "error", err)
+			http.Error(w, "", http.StatusInternalServerError)
+			return
+		}
+
+		h.Dbg("added application color tag")
+		w.WriteHeader(http.StatusOK)
+	}
+}
