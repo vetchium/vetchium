43a62d843681b668b2a232a18dfcf17d626ecd17
Sankar சங்கர் <sankar.curiosity@gmail.com>
2024-12-09 14:32:34 +0530

    Move common structs

--- typespec/common/openings.go
+++ typespec/common/openings.go
@@ -1,4 +1,4 @@
-package vetchi
+package common
 
 type OpeningType string
 
@@ -21,4 +21,21 @@ func (o OpeningType) IsValid() bool {
 		return true
 	}
 	return false
+}
+
+type EducationLevel string
+
+const (
+	BachelorEducation    EducationLevel = "BACHELOR_EDUCATION"
+	MasterEducation      EducationLevel = "MASTER_EDUCATION"
+	DoctorateEducation   EducationLevel = "DOCTORATE_EDUCATION"
+	NotMattersEducation  EducationLevel = "NOT_MATTERS_EDUCATION"
+	UnspecifiedEducation EducationLevel = "UNSPECIFIED_EDUCATION"
+)
+
+func (e EducationLevel) IsValid() bool {
+	return e == BachelorEducation || e == MasterEducation ||
+		e == DoctorateEducation ||
+		e == NotMattersEducation ||
+		e == UnspecifiedEducation
 }
