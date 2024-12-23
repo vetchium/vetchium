9da2d471413630def007576831cbd216c5be9189
Sankar சங்கர் <sankar.curiosity@gmail.com>
2024-12-23 18:34:25 +0530

    tsp as a library

--- harrypotter/src/App.tsx
+++ harrypotter/src/App.tsx
@@ -2,9 +2,17 @@ import { useState, useEffect } from "react";
 import Cookies from "js-cookie";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
+import {
+  OrgUserRoles,
+  OrgUserRole,
+  LoginRequest,
+} from "@psankar/vetchi-typespec";
 
 function App() {
   const [signedIn, setSignedIn] = useState(false);
+  const [userRole, setUserRole] = useState<OrgUserRole>(
+    OrgUserRoles.OPENINGS_VIEWER
+  );
 
   useEffect(() => {
     const cookie = Cookies.get("signedIn");
@@ -15,6 +23,7 @@ function App() {
     <Home
       onSignOut={() => {
         setSignedIn(false);
+        setUserRole(OrgUserRoles.OPENINGS_VIEWER);
         Cookies.remove("signedIn");
         window.location.href = "/";
       }}
---
+++ typespec/index.ts
@@ -0,0 +1,23 @@
+// Export common types
+export * from './common/common';
+export * from './common/openings';
+export * from './common/candidacies';
+export * from './common/interviews';
+export * from './common/applications';
+
+// Export hub types
+export * from './hub/interviews';
+export * from './hub/candidacy';
+export * from './hub/openings';
+export * from './hub/applications';
+export * from './hub/hubusers';
+
+// Export employer types
+export * from './employer/openings';
+export * from './employer/candidacy';
+export * from './employer/interviews';
+export * from './employer/orgusers';
+export * from './employer/locations';
+export * from './employer/costcenters';
+export * from './employer/applications';
+export * from './employer/auth'; 
