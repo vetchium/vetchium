839199c5ab179f866f98b0ca47f903515845f4ee
Sankar சங்கர் <sankar.curiosity@gmail.com>
2024-12-24 18:38:16 +0530

    mui support

--- ronweasly/src/app/page.tsx
+++ ronweasly/src/app/page.tsx
@@ -1,3 +1,26 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <html>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+      </head>
+      <body>
+        <h1>Hello World</h1>
+      </body>
+    </html>
+  );
 }
