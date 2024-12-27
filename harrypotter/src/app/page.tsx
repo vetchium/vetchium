34cbd4e7ac6a66a8514cce057938cb682a108aac
Sankar சங்கர் <sankar.curiosity@gmail.com>
2024-12-27 14:27:46 +0530

    mui install

--- harrypotter/src/app/page.tsx
+++ harrypotter/src/app/page.tsx
@@ -1,4 +1,8 @@
 import Image from "next/image";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 export default function Home() {
   return (
