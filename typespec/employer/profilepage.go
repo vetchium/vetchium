c9494db3664436e4b8daff3643eca1e69f2173ad
Sankar சங்கர் <sankar.curiosity@gmail.com>
2025-03-03 10:15:35 +0530

    Add WorkHistory for Employer's HubUserProfile

--- typespec/employer/profilepage.go
+++ typespec/employer/profilepage.go
@@ -1,13 +1,26 @@
 package employer
 
+import "time"
+
 type GetHubUserBioRequest struct {
 	Handle string `json:"handle" validate:"required"`
 }
 
+type EmployerWorkHistory struct {
+	ID             string     `json:"id"`
+	EmployerDomain string     `json:"employer_domain"`
+	EmployerName   *string    `json:"employer_name"`
+	Title          string     `json:"title"`
+	StartDate      time.Time  `json:"start_date"`
+	EndDate        *time.Time `json:"end_date"`
+	Description    *string    `json:"description"`
+}
+
 type EmployerViewBio struct {
-	Handle              string   `json:"handle"`
-	FullName            string   `json:"full_name"`
-	ShortBio            string   `json:"short_bio"`
-	LongBio             string   `json:"long_bio"`
-	VerifiedMailDomains []string `json:"verified_mail_domains,omitempty"`
+	Handle              string                `json:"handle"`
+	FullName            string                `json:"full_name"`
+	ShortBio            string                `json:"short_bio"`
+	LongBio             string                `json:"long_bio"`
+	VerifiedMailDomains []string              `json:"verified_mail_domains,omitempty"`
+	WorkHistory         []EmployerWorkHistory `json:"work_history"`
 }
--- typespec/employer/profilepage.ts
+++ typespec/employer/profilepage.ts
@@ -2,10 +2,21 @@ export interface GetHubUserBioRequest {
   handle: string;
 }
 
+export interface EmployerWorkHistory {
+  id: string;
+  employer_domain: string;
+  employer_name?: string;
+  title: string;
+  start_date: Date;
+  end_date?: Date;
+  description?: string;
+}
+
 export interface EmployerViewBio {
   handle: string;
   full_name: string;
   short_bio: string;
   long_bio: string;
   verified_mail_domains?: string[];
+  work_history: EmployerWorkHistory[];
 }
