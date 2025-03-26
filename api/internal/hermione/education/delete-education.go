45f7db0c84009737a4820798f6a41a2cbf2ca7a8
Sankar சங்கர் <sankar.curiosity@gmail.com>
2025-03-26 15:17:51 +0530

    Placeholder handlers

---
+++ api/internal/hermione/education/delete-education.go
@@ -0,0 +1,13 @@
+package education
+
+import (
+	"net/http"
+
+	"github.com/psankar/vetchi/api/internal/wand"
+)
+
+func DeleteEducation(h wand.Wand) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Not implemented", http.StatusNotImplemented)
+	}
+}
