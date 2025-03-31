1e5290c746415e0e479806afc947a7165ee07f00
Sankar சங்கர் <sankar.curiosity@gmail.com>
2025-03-31 16:07:28 +0530

    missing fields

--- typespec/employer/achievements.ts
+++ typespec/employer/achievements.ts
@@ -1,5 +1,7 @@
+import { AchievementType } from "../common/achievements";
 import { Handle } from "../common/common";
 
 export interface ListHubUserAchievementsRequest {
   handle: Handle;
+  type: AchievementType;
 }
