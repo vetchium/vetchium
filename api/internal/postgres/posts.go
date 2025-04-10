a9a6665618f2389f7bfedd9c7f5b34f0ffd48c05
Sankar சங்கர் <sankar.curiosity@gmail.com>
2025-04-10 23:40:03 +0530

    Fix tag insert

--- api/internal/postgres/posts.go
+++ api/internal/postgres/posts.go
@@ -37,17 +37,39 @@ VALUES ($1, $2, $3)
 		return err
 	}
 
+	tagIDs := make([]string, 0, len(req.NewTags))
+	newTagsInsertQuery := `
+INSERT INTO tags (name) VALUES ($1) RETURNING id ON CONFLICT DO NOTHING
+`
+
+	for _, tag := range req.NewTags {
+		var newTagID string
+		err = tx.QueryRow(
+			req.Context,
+			newTagsInsertQuery,
+			tag,
+		).Scan(&newTagID)
+		if err != nil {
+			pg.log.Err("failed to insert new tags", "error", err)
+			return err
+		}
+		tagIDs = append(tagIDs, newTagID)
+	}
+
+	for _, tagID := range req.TagIDs {
+		tagIDs = append(tagIDs, string(tagID))
+	}
+
 	tagsInsertQuery := `
 INSERT INTO post_tags (post_id, tag_id)
 VALUES ($1, $2) ON CONFLICT DO NOTHING
 `
-
-	for _, tag := range req.TagIDs {
+	for _, tagID := range tagIDs {
 		_, err = tx.Exec(
 			req.Context,
 			tagsInsertQuery,
 			req.PostID,
-			tag,
+			tagID,
 		)
 		if err != nil {
 			pg.log.Err("failed to insert to post_tags", "error", err)
@@ -55,7 +77,7 @@ VALUES ($1, $2) ON CONFLICT DO NOTHING
 		}
 	}
 
-	err = tx.Commit(req.Context)
+	err = tx.Commit(context.Background())
 	if err != nil {
 		pg.log.Err("failed to commit transaction", "error", err)
 		return err
