8a2bc0ed25c2fd6e2643b1b5f043d7657755e90b
Sankar சங்கர் <sankar.curiosity@gmail.com>
2025-04-16 19:02:38 +0530

    Post

--- dev-seed/broadarea-posts-map.go
+++ dev-seed/broadarea-posts-map.go
@@ -10,11 +10,26 @@ var broadAreaPostsMap map[string][]hub.AddPostRequest
 func init() {
 	broadAreaPostsMap = map[string][]hub.AddPostRequest{}
 
-	broadAreaPostsMap["Software Engineer"] = []hub.AddPostRequest{
+	broadAreaPostsMap["Engineering"] = []hub.AddPostRequest{
 		{
-			Content: "I'm a software engineer",
+			Content: `Just solved a critical production bug by turning it off and on again.
+
+They say "debugging is like being the detective in a crime movie where you are also the murderer."
+
+Today, I cleared my own name.`,
 			NewTags: []common.VTagName{
 				common.VTagName("Software Engineer"),
+				common.VTagName("Blessed"),
+			},
+		},
+		{
+			Content: `Woke up at 4AM. Did yoga. Built a microservice. Deployed to production. Got paged. Fixed it. Broke it again. Blamed Jenkins. Meditated. Asked ChatGPT. Took credit.
+
+Remember: It’s not about how many bugs you fix, it’s about how confidently you explain why they aren’t your fault.`,
+			NewTags: []common.VTagName{
+				common.VTagName("Leadership"),
+				common.VTagName("Senior Engineer"),
+				common.VTagName("Fake It Till You Deploy It"),
 			},
 		},
 	}
