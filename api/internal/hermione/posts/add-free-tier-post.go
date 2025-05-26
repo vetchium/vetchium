f1c35ab9dae6e51bfc071a8755e7df624d11e5ef
Sankar சங்கர் <sankar.curiosity@gmail.com>
2025-05-26 18:19:15 +0530

    handler

---
+++ api/internal/hermione/posts/add-free-tier-post.go
@@ -0,0 +1,54 @@
+package posts
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/vetchium/vetchium/api/internal/db"
+	"github.com/vetchium/vetchium/api/internal/util"
+	"github.com/vetchium/vetchium/api/internal/wand"
+	"github.com/vetchium/vetchium/api/pkg/vetchi"
+	"github.com/vetchium/vetchium/typespec/hub"
+)
+
+func AddFreeTierPost(h wand.Wand) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		h.Dbg("Entered AddFreeTierPost")
+		var addFTPostReq hub.AddFTPostRequest
+		if err := json.NewDecoder(r.Body).Decode(&addFTPostReq); err != nil {
+			h.Dbg("decoding failed", "error", err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if !h.Vator().Struct(w, &addFTPostReq) {
+			h.Dbg("validation failed", "addFTPostReq", addFTPostReq)
+			http.Error(w, "validation failed", http.StatusBadRequest)
+			return
+		}
+
+		h.Dbg("Validated", "addFTPostReq", addFTPostReq)
+
+		postID := util.RandomUniqueID(vetchi.PostIDLenBytes)
+
+		err := h.DB().AddFTPost(db.AddFTPostRequest{
+			Context:          r.Context(),
+			PostID:           postID,
+			AddFTPostRequest: addFTPostReq,
+		})
+		if err != nil {
+			h.Dbg("adding free tier post failed", "error", err)
+			http.Error(w, "", http.StatusInternalServerError)
+			return
+		}
+
+		err = json.NewEncoder(w).Encode(hub.AddPostResponse{
+			PostID: postID,
+		})
+		if err != nil {
+			h.Dbg("encoding failed", "error", err)
+			http.Error(w, "", http.StatusInternalServerError)
+			return
+		}
+	}
+}
